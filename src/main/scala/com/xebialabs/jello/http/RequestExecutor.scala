e76926535403a273a8c962912d4cebf20728186c
Mike Kotsur <miccots@gmail.com>
2015-02-22 22:00:34 +0100

    Small cleanup

--- src/main/scala/com/xebialabs/jello/http/RequestExecutor.scala
+++ src/main/scala/com/xebialabs/jello/http/RequestExecutor.scala
@@ -1,22 +1,14 @@
 package com.xebialabs.jello.http
 
-import akka.actor.ActorSystem
-import akka.util.Timeout
 import com.typesafe.scalalogging.LazyLogging
-import com.xebialabs.jello.http.RequestExecutor._
-import com.xebialabs.jello.system.dispatcher
 import com.xebialabs.jello.system
+import com.xebialabs.jello.system.dispatcher
 import spray.client.pipelining._
 import spray.http._
 import spray.httpx.unmarshalling._
 
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
-object RequestExecutor {
-
-
-}
 
 trait RequestExecutor extends LazyLogging {
 
